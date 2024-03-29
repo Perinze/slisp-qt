ffa79c78ef043c46b2b894090e28ac1bc40da431
perinze <perinzefaper@foxmail.com>
2024-03-30 02:48:37 +0800

    migrate tokenize

--- tokenize.cpp
+++ tokenize.cpp
@@ -6,7 +6,52 @@
 TokenSequenceType tokenize(std::istream & seq){
   TokenSequenceType tokens;
 
-  // TODO: implement your code here
+  char c;
+  std::string tmp;
+  bool on_atom = false;
+
+  auto commit_atom_if_leaving = [&]() {
+    if (on_atom) {
+      tokens.push_back(tmp);
+      on_atom = false;
+    }
+  };
+
+  while (seq.get(c)) {
+    switch (c)
+    {
+    case OPEN:
+      commit_atom_if_leaving();
+      tokens.push_back("(");
+      break;
+
+    case CLOSE:
+      commit_atom_if_leaving();
+      tokens.push_back(")");
+      break;
+
+    case COMMENT:
+      commit_atom_if_leaving();
+      while (seq.get(c) && c != '\n');
+      break;
+
+    case ' ':
+    case '\t':
+    case '\r':
+    case '\n':
+      commit_atom_if_leaving();
+      break;
+
+    default:
+      if (on_atom) { // continue reading to tmp
+        tmp.push_back(c);
+      } else { // start reading to tmp
+        tmp = c;
+        on_atom = true;
+      }
+    }
+  }
+  commit_atom_if_leaving();
 
   return tokens;
 }
